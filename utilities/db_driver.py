6a70e56407d18356fe43957485b5ac0eab46df70
Ian Fraser <33810966+ianFraser858@users.noreply.github.com>
2019-12-23 15:34:14 -0600

    added go between to db2 on z-OS

--- utilities/db_driver.py
+++ utilities/db_driver.py
@@ -9,17 +9,50 @@ class DbDriver:
         self.metadata = MetaData()
         self.conn = db2.connect()
 
-        def get_table_cols(self):
-            set_schema('SYSIBM')
+    def get_table_cols(self, schema, table):
+        dict_of_cols = {}
 
-            table = Table(
-                'SYSCOLUMNS', db2.metadata,
-                Column('NAME', String),
-                Column('TBNAME', String),
-                Column('TBCREATOR', String),
-                Column('COLTYPE', String))
+        created_table = Table(
+            'SYSCOLUMNS', self.metadata,
+            Column('NAME', String),
+            Column('TBNAME', String),
+            Column('TBCREATOR', String),
+            Column('COLTYPE', String),
+            extend_existing =True,
+            schema='SYSIBM')
 
-            print('add code here')
+        s = created_table.select().where(created_table.c.TBCREATOR == schema).where(created_table.c.TBNAME == table)
 
-        def set_schema(self, schema):
-            return self.conn.execute('set schema {}'.format(schema))
+        result = self.conn.execute(s)
+
+        for entry in result:
+            dict_of_cols[entry[0]] = Column(entry[0])
+
+        return dict_of_cols
+
+    def get_all_from_table(self, schema, table):
+        all_cols = self.get_table_cols(schema, table)
+
+        created_table = Table(
+            table,
+            self.metadata,
+            extend_existing=True,
+            schema=schema)
+
+        for entry in all_cols:
+            created_table.append_column(all_cols[entry])
+
+        query = created_table.select()
+        result = self.conn.execute(query)
+
+        return result
+
+    def create_table(self, schema, table):
+        return Table(
+            table,
+            self.metadata,
+            extend_existing=True,
+            schema=schema)
+
+    def set_schema(self, schema):
+        return self.conn.execute('set schema {}'.format(schema))
